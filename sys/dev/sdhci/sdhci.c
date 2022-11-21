f315ff3a6ff1f54172d8732e6091936dec107497
Michal Meloun <mmel@FreeBSD.org>
2022-11-21 14:55:43 +0100

    Revert "sdhci:  add sdhci_prepare_clock method"
    This reverts commit ee765a9c72cc763376e158092975eae6e8b590fb.

--- sys/dev/sdhci/sdhci.c
+++ sys/dev/sdhci/sdhci.c
@@ -404,8 +404,8 @@ sdhci_init(struct sdhci_slot *slot)
 	WR4(slot, SDHCI_SIGNAL_ENABLE, slot->intmask);
 }
 
-int sdhci_generic_prepare_clock(device_t brdev, struct sdhci_slot *slot,
-     uint32_t clock)
+static void
+sdhci_set_clock(struct sdhci_slot *slot, uint32_t clock)
 {
 	uint32_t clk_base;
 	uint32_t clk_sel;
@@ -414,13 +414,17 @@ int sdhci_generic_prepare_clock(device_t brdev, struct sdhci_slot *slot,
 	uint16_t div;
 	int timeout;
 
-<<<<<<< HEAD
 	if (clock == slot->clock)
 		return;
-	clock = SDHCI_SET_CLOCK(slot->bus, slot, clock);
-=======
->>>>>>> 04706ff01f4f (sdhci:  add sdhci_prepare_clock method)
 	slot->clock = clock;
+
+	/* Turn off the clock. */
+	clk = RD2(slot, SDHCI_CLOCK_CONTROL);
+	WR2(slot, SDHCI_CLOCK_CONTROL, clk & ~SDHCI_CLOCK_CARD_EN);
+	/* If no clock requested - leave it so. */
+	if (clock == 0)
+		return;
+
 	/* Determine the clock base frequency */
 	clk_base = slot->max_clk;
 	if (slot->quirks & SDHCI_QUIRK_BCM577XX_400KHZ_CLKSRC) {
@@ -479,7 +483,7 @@ int sdhci_generic_prepare_clock(device_t brdev, struct sdhci_slot *slot,
 		<< SDHCI_DIVIDER_HI_SHIFT;
 
 	WR2(slot, SDHCI_CLOCK_CONTROL, clk);
-	/* Enable internal clock generator. */
+	/* Enable clock. */
 	clk |= SDHCI_CLOCK_INT_EN;
 	WR2(slot, SDHCI_CLOCK_CONTROL, clk);
 	/* Wait up to 10 ms until it stabilize. */
@@ -490,35 +494,12 @@ int sdhci_generic_prepare_clock(device_t brdev, struct sdhci_slot *slot,
 			slot_printf(slot,
 			    "Internal clock never stabilised.\n");
 			sdhci_dumpregs(slot);
-			return(EIO);
+			return;
 		}
 		timeout--;
 		DELAY(1000);
 	}
-	return(0);
-}
-
-static void
-sdhci_set_clock(struct sdhci_slot *slot, uint32_t clock)
-{
-	uint16_t clk;
-
-	if (clock == slot->clock)
-		return;
-	slot->clock = clock;
-
-	/* Turn off the clock. */
-	clk = RD2(slot, SDHCI_CLOCK_CONTROL);
-	WR2(slot, SDHCI_CLOCK_CONTROL, clk & ~SDHCI_CLOCK_CARD_EN);
-
-	/* If no clock requested - leave it so. */
-	if (clock == 0)
-		return;
-
-	SDHCI_PREPARE_CLOCK(slot->bus, slot, clock);
-
 	/* Pass clock signal to the bus. */
-	clk = RD2(slot, SDHCI_CLOCK_CONTROL);
 	clk |= SDHCI_CLOCK_CARD_EN;
 	WR2(slot, SDHCI_CLOCK_CONTROL, clk);
 }
