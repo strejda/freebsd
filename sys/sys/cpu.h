strejda/freebsd

commit c3c8f4d9e66287b24af2b3881c528236b836f2f1

cpu: New cpu_get_pcpuid(), retrieves internal CPU ID
It is a handy shortcut that will be used extensively in
hwpstate_intel(4) and hwpstate_amd(4).

Warn users that it panics if the parent bus does not provide the
CPU_IVAR_PCPU instance variable.  That condition should be tested by
callers (doing so once is enough).  Suggest to do that in driver's
attach method.

Reviewed by:    jhb (code)
Event:          Halifax Hackathon 202606
Location:       Seat 36K in AC667, waiting for a gate at Montréal-Trudeau
Sponsored by:   The FreeBSD Foundation
Differential Revision:  https://reviews.freebsd.org/D57897

sys/sys/cpu.h
@@ -51,6 +51,17 @@ cpu_get_pcpu(device_t dev)
 	return ((struct pcpu *)v);
 }
 
+/*
+ * Assumes the parent bus sets the CPU_IVAR_PCPU instance variable, which most
+ * CPU buses do.  A panic will ensure if not the case.  Calling devices should
+ * check for that condition (most probably in their attach function).
+ */
+static __inline u_int
+cpu_get_pcpuid(device_t dev)
+{
+	return (cpu_get_pcpu(dev)->pc_cpuid);
+}
+
 static __inline int32_t
 cpu_get_nominal_mhz(device_t dev)
 {

commit 302d3a21ad9aae26a55aa8d8396c69f8ca8ac33e

cpu: cpu_get_pcpuid(): Fix typo in herald comment
Fixes:          c3c8f4d9e662 ("cpu: New cpu_get_pcpuid(), retrieves internal CPU ID")
Sponsored by:   The FreeBSD Foundation

sys/sys/cpu.h
@@ -53,7 +53,7 @@ cpu_get_pcpu(device_t dev)
 
 /*
  * Assumes the parent bus sets the CPU_IVAR_PCPU instance variable, which most
- * CPU buses do.  A panic will ensure if not the case.  Calling devices should
+ * CPU buses do.  A panic will ensue if not the case.  Calling devices should
  * check for that condition (most probably in their attach function).
  */
 static __inline u_int